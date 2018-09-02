5d135025ff18ba1f7095deed483760f7894c6855
Saurabh Singh <saurabhsinghmay99@gmail.com>
2018-09-02 16:08:46 +0530

    init

---
+++ Question2.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+	
+public:
+	int data; 
+	Node* next;
+	Node* prev;
+	Node();
+	Node(int data1, Node* previousNode){
+		data = data1;
+		next = NULL;
+		prev = previousNode;
+	}
+};
+
+void append(Node** head, int newData) {
+	if (*head == NULL)
+	{
+		*head = new Node(newData, NULL);
+	}
+	else {
+		Node* previousNode;
+		Node* nextNode;
+		previousNode = *head;
+		while(previousNode->next != NULL){
+			previousNode = previousNode->next;
+		}
+		Node* newNode = new Node(newData, previousNode);
+		previousNode->next = newNode;
+	}
+}
+
+void printList(Node** head) {
+	Node* nextNode;
+	nextNode = *head;
+
+	cout<< "The list is"<<endl;
+
+	while(nextNode != NULL){
+		cout << nextNode->data << endl;
+		nextNode = nextNode->next;
+	}
+}
+
+bool checkforthree(Node** current){
+	int i=1;
+	Node* curr = *current;
+	while(curr->next != NULL && i<=3){
+		i++;
+		curr = curr->next;
+	}
+	if(i==3) return true;
+	else return false;
+
+}
+
+void swapthree(Node** current){
+	Node* first= *current;
+	Node* secont= first->next;
+	Node* third= second->next;
+	Node* copyOfFirst= first;
+
+	first = third;
+	third = copyOfFirst;
+
+}
+int main()
+{
+	Node* head  = NULL;
+
+	cout<<"Enter the length of linked list"<< endl;
+	int length;
+	cin>>length;
+	cout<<"Enter the values"<< endl;
+
+	for (int i = 0; i < length; ++i)
+	{
+		int k;
+		cin>>k;
+		append(&head, k);
+	}
+
+	printList(&head);
+	
+
+	return 0;
+}
